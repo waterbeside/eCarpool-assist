17a4ada58255630246e90088cf625837c638d044
waterbeside <waterbeside@163.com>
2018-03-28 22:15:05 +0800

    feat: add vue components (StatisItem)

---
+++ carpool_assist/src/components/StatisItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+     <b v-if="title" class="title">{{title}}</b>
+     <slot name="title"></slot>
+     <slot name="icon"></slot>
+     <i class="cp-iconfont" :class="icon" v-if="icon"></i>
+     <div class="num_unit_wrapper">
+       <slot name="num"></slot>
+       <countup v-if="typeof(num)!='undefined'" :start-val="0" :end-val="num" :duration="duration" class="num"></countup>
+       <b class="unit"  v-if="unit">{{unit}}</b>
+     </div>
+
+   </div>
+
+</template>
+
+<script>
+import { Countup } from 'vux'
+
+export default {
+  components: {
+    Countup
+  },
+  data () {
+    return {
+    }
+  },
+  props: {
+    title:String,
+    num : {},
+    icon : String,
+    unit : String,
+    duration : {
+      default:2
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped >
+
+
+</style>
