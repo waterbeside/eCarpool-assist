2caf6bc6358abc1d7d2456fc0f2bb2a858c74fd6
waterbeside <waterbeside@163.com>
2018-07-03 11:52:31 +0800

    opt: 添加导航

--- assist_vue/src/modules/main/pages/index.vue
+++ assist_vue/src/modules/main/pages/index.vue
@@ -2,6 +2,12 @@
   <view-box ref="viewBox" >
 
         <div class="page-view-content">
+          <ul class="list-group">
+            <li class="list-group-item"><a href="aboutus.html#/qcode">关于我们</a></li>
+            <li class="list-group-item"><a href="docs.html">文档</a></li>
+            <li class="list-group-item"><a href="invitation.html">站外邀请</a></li>
+            <li class="list-group-item"><a href="reports.html">报表</a></li>
+          </ul>
 
         </div>
    </view-box>
@@ -14,11 +20,30 @@ import config from '../../../configs'
 export default {
   components: {
 
-
   },
 
   data () {
     return {
+      list: [{
+        src: 'http://somedomain.somdomain/x.jpg',
+        fallbackSrc: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+        title: '标题一',
+        desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
+        url: '/component/cell'
+      }, {
+        src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+        title: '标题二',
+        desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
+        url: {
+          path: '/component/radio',
+          replace: false
+        },
+        meta: {
+          source: '来源信息',
+          date: '时间',
+          other: '其他信息'
+        }
+      }],
 
 
     }
@@ -37,7 +62,7 @@ export default {
 
   },
   activated (){
-   
+
   }
 }
 
