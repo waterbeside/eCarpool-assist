4f3b116be7e41db0f39f7c8f3df97039732338a6
waterbeside <waterbeside@163.com>
2019-07-01 08:38:03 +0800

    feat: “通过id取得部门信息”方法

--- assist_vue/src/api/department.js
+++ assist_vue/src/api/department.js
@@ -1,8 +1,12 @@
 import axios from '@/utils/axios'
 import configs from '@/configs'
+import myCache from "@/utils/myCache";
 
 export default {
-  getListByDeep(params){
+  getListByDeep(params,setting = {cacheKey:false,exp:3600}){
+    if(typeof(setting.cacheKey) !="undefined" && typeof(setting.exp) !="undefined" && setting.cacheKey){
+      return myCache.do(setting.cacheKey,[this.getListByDeep,params],setting.exp);
+    }
     return new Promise((resole,reject)=>{
       axios({
         url: configs.baseUrl_8082+'/api/v1/department/selects',
@@ -14,6 +18,26 @@ export default {
         }else{
           reject(res);
         }
+      }).catch(error=>{
+        reject(error);
+      })
+    })
+  },
+
+  
+  getDeep3ItemData(id){
+    return new Promise((resole,reject)=>{
+      this.getListByDeep({ deep: 3 },{cacheKey:"carpool:departmet:selects:deep3",exp:3600}).then(res=>{
+        let lists = res;
+        let returnData = null;
+        for(let i in lists){
+          let item = lists[i];
+          if(item.id == id){
+            returnData = item;
+            break;
+          }
+        } 
+        resole(returnData);      
       }).catch(error=>{
         reject(error);
       })
